7aea1ada6e9ac27e4fb1c6e33181c1223ad40b23
Dawid <dawnap000@utp.edu.pl>
2022-05-30 00:04:18 +0200

    LoginPage

--- spotify-2-0/pages/login.js
+++ spotify-2-0/pages/login.js
@@ -7,7 +7,8 @@ function Login({providers}){
                 <div key={provider.name}>
                     <button className="bg-[#18D860] text-white p-5 rounded-full"
                     onClick={() => signIn(provider.id, {callbackUrl: "/" }
-                     )}>
+                     )}
+                     >
                     Login with {provider.name}
                     </button>
                     </div>
