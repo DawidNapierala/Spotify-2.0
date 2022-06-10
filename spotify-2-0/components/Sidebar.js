c6f069a9450879f118f3083bb3a3fac57b120a78
Maciuee <mmusial125@gmail.com>
2022-06-10 21:45:16 +0200

    working 2.15

--- spotify-2-0/components/Sidebar.js
+++ spotify-2-0/components/Sidebar.js
@@ -9,9 +9,26 @@ import {
     PlusCircleIcon,
 } from "@heroicons/react/solid";
 import { signOut, useSession} from "next-auth/react";
+import { useEffect, useState } from "react";
+import useSpotify from "../hooks/useSpotify";
+
+
 
 function Sidebar() {
+    const spotifyApi = useSpotify();
     const { data: session, status } = useSession();
+    const [playlists, setPlaylists] = useState([]);
+
+    useEffect(() => {
+        if(spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPlaylists(data.body.items);
+            });
+        }
+
+    }, [session, spotifyApi]);
+
+    console.log(playlists);
     
     return (
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
