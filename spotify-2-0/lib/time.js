dc374150b1130969eaaa4a0f6e3eb865ef83830c
Natalia020 <59650296+Natalia020@users.noreply.github.com>
2022-06-12 16:22:44 +0200

    Delete spotify-2-0 directory

--- spotify-2-0/lib/time.js
+++
@@ -1,8 +0,0 @@
-export function millisToMinutesAndSecons(millis) {
-    const minutes = Math.floor(millis /60000);
-    const seconds = ((millis % 60000) /1000).toFixed(0);
-    return seconds == 60
-    ? minutes + 1 + ":00"
-    : minutes + ":" +(seconds <10 ? "0" : "") +
-    seconds;
-}
